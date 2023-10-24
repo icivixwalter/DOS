c0dbef14c7a9fdc6add2b41928d4487f8216cd55
walter <47453186+icivixwalter@users.noreply.github.com>
2023-10-24 10:33:06 +0200

    salvato in ufficio il progetto split

--- GIT_BAT/git_OpenGitHub.bat
+++ GIT_BAT/git_OpenGitHub.bat
@@ -12,6 +12,20 @@ start https://github.com/icivixwalter
 
 
 
+goto Utilita_git
+
+	note
+		descrizione di comandi veloci in git
+
+	C 
+		CLONARE UNA REPOSITORY 
+			esempio clono la repository  del Dos (attenzione la cartella non deve essistere nella
+			directotory dove viene effettuato il clone): 
+
+				git clone https://github.com/icivixwalter/DOS.git
+
+
+:Utilita_git
 
 
 
@@ -20,16 +34,15 @@ start https://github.com/icivixwalter
 
 
 
-goto errori_git
 
-A 
-	ALREADY
-		Alreade up to date
-			Già aggiornato
 
-C 
-	COMMIT
-			nothing to commit, working tree clean
-				niente da impegnare, albero funzionante pulito
 
-:errori_git
+
+
+
+
+
+
+
+
+
